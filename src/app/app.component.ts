2c05a1f5a6dbba33bcac4393277d7d8d535b6771
S <ns1013@LAMU02WP3A8HTD6.uhc.com>
2019-09-04 18:15:23 +0530

    bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,43 @@ export class AppComponent {
   promptEvent;
 
   constructor() {
+    console.log('before beforeinstallprompt ',event);
+
     window.addEventListener('beforeinstallprompt', event => {
       console.log('invoked beforeinstallprompt ',event);
-
+      event.preventDefault();
       this.promptEvent = event;
+    });
+
+    window.addEventListener('appinstalled', (evt) => {
+      console.log("appinstalled successfully");
     });
   }
 
   installPwa(): void {
-    console.log('installPwa');
+    console.log('installPwa method ');
     console.log(this.promptEvent);
+    if(this.promptEvent !== undefined) {
+      // The user has had a postive interaction with our app and Chrome
+      // has tried to prompt previously, so let's show the prompt.
+      this.promptEvent.prompt();
+
+      // Follow what the user has done with the prompt.
+      this.promptEvent.userChoice.then(function(choiceResult) {
+
+        console.log(choiceResult.outcome);
+
+        if(choiceResult.outcome == 'dismissed') {
+          console.log('App cancelled home screen install');
+        }
+        else {
+          console.log('App added to home screen');
+        }
+
+        // We no longer need the prompt.  Clear it up.
+        this.promptEvent = null;
+      });
+    }
   }
 
 }
