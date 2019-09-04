773dc93911317598a123edf360e3863119eac76a
S <ns1013@LAMU02WP3A8HTD6.uhc.com>
2019-09-04 17:53:51 +0530

    code updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { PwaService } from './services/pwa/pwa.service';
 
 @Component({
   selector: 'gs-root',
@@ -8,48 +7,13 @@ import { PwaService } from './services/pwa/pwa.service';
 })
 export class AppComponent {
   title = 'g-store';
-  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
-  showBtn: boolean = false;
-  deferredPrompt;
 
-  constructor(public pwa: PwaService) {
+  constructor() {
 
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later on the button event.
-      this.deferredPrompt = e;
-
-      // Update UI by showing a button to notify the user they can add to home screen
-      this.showBtn = true;
-    });
-
-    //button click event to show the promt
-
-    window.addEventListener('appinstalled', (event) => {
-      alert('installed');
-    });
-
-
-    if (window.matchMedia('(display-mode: standalone)').matches) {
-      alert('display-mode is standalone');
-    }
   }
 
   installPwa(): void {
-    // hide our user interface that shows our button
-    // Show the prompt
-    this.deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    this.deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          alert('User accepted the prompt');
-        } else {
-          alert('User dismissed the prompt');
-        }
-        this.deferredPrompt = null;
-      });
+    console.log('installPwa');
   }
 
 }
